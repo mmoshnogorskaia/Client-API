59c3f3aea88bc806aae684afe84e6edb006d054d
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2019-10-29 19:48:36 +0300

    Added main modules and components

---
+++ src/app/modules/main-page/main-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChartComponent } from './chart/chart.component';
+import { MainPageRoutingModule } from './main-page-routing.module';
+
+
+@NgModule({
+  declarations: [ChartComponent],
+  imports: [
+    CommonModule,
+    MainPageRoutingModule,
+  ]
+})
+export class MainPageModule { }
